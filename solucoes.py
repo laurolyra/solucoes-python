b9b3c8d3df135fadec280d5b6c0447c05d96d897
Lauro Lyra <laurolyra@gmail.com>
2025-04-08 21:09:15 -0300

    copia de enunciados

---
+++ solucoes.py
@@ -0,0 +1,7 @@
+#Crie uma lista com as strings 'infinity', 'school', 'curso', 'dfs'.
+#Insira a string 'python' na lista do exercício acima.
+#Imprima a lista após remover o primeiro elemento dela.
+#Imprima a lista após remover o último elemento dela.
+#Defina que o terceiro elemento da lista será 'course' e imprima a lista.
+#Com a lista ['Mike', '', 'Emma', 'Kelly', '', 'Brad'], imprima a lista sem as strings vazias.
+
